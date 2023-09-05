10a45d5363ea707f54881db352ddfc26c025c320
chankjc <r11922032@csie.ntu.edu.tw>
2023-09-05 15:05:46 +0000

    add dataset

--- train_embedding.py
+++ train_embedding.py
@@ -1,9 +1,23 @@
 import os
 
 from dotenv import load_dotenv
-from torch_geometric.datasets import Planetoid
+from torch_geometric.datasets import Actor, Planetoid
 
 load_dotenv()
 
-dataset = Planetoid(root=os.environ["DATASET_DIR"], name="CiteSeer")
-breakpoint()
+
+def prepare_dataset():
+    dataset = {}
+    dataset["cora"] = Planetoid(root=os.environ["DATASET_DIR"], name="Cora")
+    dataset["citeseer"] = Planetoid(root=os.environ["DATASET_DIR"], name="CiteSeer")
+    dataset["actor"] = Actor(root=os.environ["DATASET_DIR"])
+    return dataset
+
+
+def main():
+    dataset = prepare_dataset()
+    print(len(dataset))
+
+
+if __name__ == "__main__":
+    main()
