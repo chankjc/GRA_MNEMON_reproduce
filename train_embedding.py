ee5c2c94ab1fd0350b73b2119429b3108a64057b
chankjc <r11922032@csie.ntu.edu.tw>
2023-09-05 15:45:52 +0000

    add facebook dataset

--- train_embedding.py
+++ train_embedding.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from torch_geometric.datasets import Actor, Planetoid
+from torch_geometric.datasets import Actor, FacebookPagePage, Planetoid
 
 load_dotenv()
 
@@ -11,12 +11,13 @@ def prepare_dataset():
     dataset["cora"] = Planetoid(root=os.environ["DATASET_DIR"], name="Cora")
     dataset["citeseer"] = Planetoid(root=os.environ["DATASET_DIR"], name="CiteSeer")
     dataset["actor"] = Actor(root=os.environ["DATASET_DIR"])
+    dataset["facebook"] = FacebookPagePage(root=os.environ["DATASET_DIR"])
     return dataset
 
 
 def main():
     dataset = prepare_dataset()
-    print(len(dataset))
+    print(dataset["cora"].x)
 
 
 if __name__ == "__main__":
