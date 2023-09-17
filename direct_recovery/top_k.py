b16bdd65a1745ecf5001844c4da4f03237b88d95
chankjc <r11922032@csie.ntu.edu.tw>
2023-09-17 20:24:05 +0800

    update for many thins

---
+++ direct_recovery/top_k.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn.functional as F
+
+
+def distance(x, dist):
+    if dist == "cosine":
+        x_norm = F.normalize(x, p=2, dim=1)
+        result = torch.mm(x_norm, x_norm.transpose(0, 1))
+    return result
+
+def top_k(x, k, dist):
+    edge = [[],[]]
+    num_node = x.shape[0]
+    pairwire = distance(x, dist)
+    values, indices = torch.topk(pairwire.flatten(), (k * num_node) // 2)
+
+    for ind in indices:
+        edge[1].append(ind // num_node)
+        edge[0].append(ind % num_node)
+
+    return edge
---
+++ mnemon/prepare_dataset.py
@@ -0,0 +1,45 @@
+import os
+import os.path as osp
+
+import torch
+import torch_geometric.transforms as T
+from dotenv import load_dotenv
+from torch_geometric.datasets import Actor, FacebookPagePage, Planetoid
+
+
+def prepare_dataset(name = "cora", algo = "gcn", device = "cuda:0"):
+    
+    transform = T.Compose(
+        [
+            T.NormalizeFeatures(),
+            T.ToDevice(device),
+            T.RandomLinkSplit(
+                num_val=0.05,
+                num_test=0.1,
+                is_undirected=True,
+                split_labels=True,
+                add_negative_train_samples=False,
+            ),
+        ]
+    )
+
+    datasets = {}
+    embeddings = {}
+    datasets["cora"] = Planetoid(
+        root=os.environ["DATASET_DIR"], name="Cora", transform=transform
+    )
+    embeddings["cora"] = torch.load(f"{os.environ['EMBEDDING_DIR']}{algo}/cora/data.pt")
+    datasets["citeseer"] = Planetoid(
+        root=os.environ["DATASET_DIR"], name="CiteSeer", transform=transform
+    )
+    embeddings["citeseer"] = torch.load(f"{os.environ['EMBEDDING_DIR']}{algo}/citeseer/data.pt")
+    datasets["actor"] = Actor(
+        root=os.environ["DATASET_DIR"] + "/Actor", transform=transform
+    )
+    embeddings["actor"] = torch.load(f"{os.environ['EMBEDDING_DIR']}{algo}/actor/data.pt")
+    datasets["facebook"] = FacebookPagePage(
+        root=os.environ["DATASET_DIR"] + "/Facebook", transform=transform
+    )
+    embeddings["facebook"] = torch.load(f"{os.environ['EMBEDDING_DIR']}{algo}/facebook/data.pt")
+    
+    return datasets[name], embeddings[name], datasets[name].x, datasets[name].edge_index
